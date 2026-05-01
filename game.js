ee0cba6cea9a4e30419916355c7ecb6dfcfee28c
Denis-Gamolya <denis.gamolya@gmail.com>
2026-04-30 20:50:31 -0700

    Update Outro scene text to enhance player experience

--- game.js
+++ game.js
@@ -526,7 +526,7 @@ class Outro extends Phaser.Scene {
         super('outro');
     }
     create() {
-        this.add.text(50, 50, "That's all!").setFontSize(50);
+        this.add.text(50, 50, "Bathe in the honey").setFontSize(50);
         this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
         this.input.on('pointerdown', () => this.scene.start('WorldSpawn'));
 
